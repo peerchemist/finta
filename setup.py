519264d6e7fef58839b635a26f8a22194140de9b
Peerchemist <dx.1178@gmail.com>
2018-03-10 19:55:23 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='finta',
-      version='0.2.6',
+      version='0.2.7',
       description=' Common financial technical indicators implemented in Pandas.',
       url='https://github.com/peerchemist/finta',
       author='Peerchemist',
       author_email='peerchemist@protonmail.ch',
       license='GLP',
       packages=['finta'],
-      install_requires=['pandas'],
-      zip_safe=False)
+      install_requires=['pandas']
+      )

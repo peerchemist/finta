38beeb1695bc3b326693f22380ae0481dcbf80ad
Peerchemist <dx.1178@gmail.com>
2018-06-13 12:08:33 +0200

    prepare for next release

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
+classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Financial and Insurance Industry',
+  'Programming Language :: Python',
+  'Operating System :: OS Independent',
+  'Natural Language :: English',
+  'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+]
+
 setup(name='finta',
-      version='0.2.7',
+      version='0.2.8',
       description=' Common financial technical indicators implemented in Pandas.',
       url='https://github.com/peerchemist/finta',
       author='Peerchemist',
       author_email='peerchemist@protonmail.ch',
-      license='GLP',
+      license='LGPLv3+',
       packages=['finta'],
       install_requires=['pandas']
       )

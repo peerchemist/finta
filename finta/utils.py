39170b60c93aab2e81d86ff0751223f56a7a2f59
Peerchemist <dx.1178@gmail.com>
2019-05-05 13:06:18 +0200

    added utils for support operations

---
+++ finta/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def to_dataframe(ticks: list) -> pd.DataFrame:
+    """Convert list to Series compatible with the library."""
+
+    df = pd.DataFrame(ticks)
+    df.set_index("time", inplace=True)
+
+    return df
+
+
+def resample(df: pd.DataFrame, interval: str) -> pd.DataFrame:
+    """Resample DataFrame by <interval>."""
+
+    d = {"open": "first", "high": "max", "low": "min", "close": "last", "volume": "sum"}
+
+    return df.resample(interval).agg(d)

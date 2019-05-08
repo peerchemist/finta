c72bfbc3333a66ba851907992a2e62582242bd07
Peerchemist <dx.1178@gmail.com>
2019-05-08 15:48:52 +0200

    implement trending_up and trending_down

--- finta/utils.py
+++ finta/utils.py
@@ -17,3 +17,23 @@ def resample(df: pd.DataFrame, interval: str) -> pd.DataFrame:
     d = {"open": "first", "high": "max", "low": "min", "close": "last", "volume": "sum"}
 
     return df.resample(interval).agg(d)
+
+
+def trending_up(df: pd.Series, period: int) -> pd.Series:
+    """returns boolean Series if the inputs Series is trending up over last n periods.
+    :param df: data
+    :param period: range
+    :return: result Series
+    """
+
+    return pd.Series(df.diff(period) > 0, name="trending_up {}".format(period))
+
+
+def trending_down(df: pd.Series, period: int) -> pd.Series:
+    """returns boolean Series if the input Series is trending up over last n periods.
+    :param df: data
+    :param period: range
+    :return: result Series
+    """
+
+    return pd.Series(df.diff(period) < 0, name="trending_down {}".format(period))
